c3b4473440c37e8a4f0ae1f9fb4fe67630a05fa8
Sanmountain <tksgk7777@naver.com>
2022-10-27 16:07:24 +0900

    1027 ing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
+  const [shoes] = useState(data);
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to="/">Home</Link>
+            <Link to="/cart">Cart</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<div></div>} />
+        <Route path="/detail" element={<div></div>} />
+        <Route />
+      </Routes>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i + 1}></Card>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+        alt="shoes1"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
